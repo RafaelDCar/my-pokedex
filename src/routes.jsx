2b18abd9c1bc80e59e40ab2a293277fdf1f254c7
Rafael <r11carvalho@gmail.com>
2021-07-17 15:33:35 -0300

    add graphql and attempt test to use route in ghpages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 import Home from './pages/home/home';
 import Pokedex from './pages/pokedex/pokedex';
@@ -8,10 +8,10 @@ import Pokedex from './pages/pokedex/pokedex';
 const RouterApp = () => {
 
   return (
-    <>
+    <HashRouter basename='/'>
       <Route path='/' exact component={Home} />
       <Route path='/pokedex' component={Pokedex} />
-    </>
+    </HashRouter >
   )
 }
 
